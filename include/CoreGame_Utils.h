e5e0a2b6faf64698932bcafeb93ad45c16a7c70b
n2omatt <n2o.matt@gmail.com>
2016-05-17 21:24:30 -0300

    [Version Update v0.0.3] include/Macros/CoreGame_Disallow.h     - Fix the macro name.     - Add the other disallow macros.

--- include/CoreGame_Utils.h
+++ include/CoreGame_Utils.h
@@ -52,8 +52,8 @@
 //The core version number.
 #define COW_COREGAME_VERSION_MAJOR    "0"
 #define COW_COREGAME_VERSION_MINOR    "0"
-#define COW_COREGAME_VERSION_REVISION "2"
+#define COW_COREGAME_VERSION_REVISION "3"
 
-#define COW_COREGAME_VERSION "0.0.2"
+#define COW_COREGAME_VERSION "0.0.3"
 
 #endif // defined(__CoreGame_include_CoreGame_Utils_h__) //
