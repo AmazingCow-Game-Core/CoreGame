9e4738c1d46ef3cda5c217329392bd79283048ad
n2omatt <n2omatt@amazingcow.com>
2018-03-29 18:33:30 -0300

    - Add some header files.

---
+++ CoreGame/include/CoreGame_Utils.h
@@ -0,0 +1,41 @@
+//~---------------------------------------------------------------------------//
+//                     _______  _______  _______  _     _                     //
+//                    |   _   ||       ||       || | _ | |                    //
+//                    |  |_|  ||       ||   _   || || || |                    //
+//                    |       ||       ||  | |  ||       |                    //
+//                    |       ||      _||  |_|  ||       |                    //
+//                    |   _   ||     |_ |       ||   _   |                    //
+//                    |__| |__||_______||_______||__| |__|                    //
+//                             www.amazingcow.com                             //
+//  File      : CoreGame_Utils.h                                              //
+//  Project   : CoreGame                                                      //
+//  Date      : Mar 29, 2018                                                  //
+//  License   : GPLv3                                                         //
+//  Author    : n2omatt <n2omatt@amazingcow.com>                              //
+//  Copyright : AmazingCow - 2018                                             //
+//                                                                            //
+//  Description :                                                             //
+//                                                                            //
+//---------------------------------------------------------------------------~//
+
+#pragma once
+
+//----------------------------------------------------------------------------//
+// Namespace                                                                  //
+//----------------------------------------------------------------------------//
+#define NS_COREGAME_BEGIN namespace CoreGame {
+#define NS_COREGAME_END   }
+#define USING_NS_COREGAME using namespace CoreGame
+
+
+//----------------------------------------------------------------------------//
+// Version                                                                    //
+//----------------------------------------------------------------------------//
+#define COW_COREGAME_VERSION_MAJOR    "0"
+#define COW_COREGAME_VERSION_MINOR    "0"
+#define COW_COREGAME_VERSION_REVISION "13"
+
+#define COW_COREGAME_VERSION       \
+    COW_COREGAME_VERSION_MAJOR "." \
+    COW_COREGAME_VERSION_MINOR "." \
+    COW_COREGAME_VERSION_REVISION
